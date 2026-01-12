3b9f369a6de919deaffb42e7572f1bdfe86fc3e7
Ahmed Mohamed <153910180+Ahmedianoo@users.noreply.github.com>
2026-01-13 00:39:01 +0200

    Update signup.jsx

--- frontend/sales-assistant/src/app/components/auth/signup.jsx
+++ frontend/sales-assistant/src/app/components/auth/signup.jsx
@@ -20,7 +20,7 @@ export default function SignupPage() {
     setMessage("");
 
     try {
-      const res = await axios.post("http://localhost:8000/users/signup", form);
+      const res = await axios.post("https://ahmedianoo-ai-sales-assistant-backend.hf.space/users/signup", form);
       localStorage.setItem("jwt", res.data.token);
       router.push("/home");
 
